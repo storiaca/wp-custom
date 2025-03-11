312bd9ffddd3c72d2873f944932ec2c865b380bc
Aleksandar Ristic <storiaca@gmail.com>
2025-03-11 14:39:35 +0100

    PHP Functions

--- fictional-univeristy-theme/index.php
+++ fictional-univeristy-theme/index.php
@@ -1,1 +1,20 @@
-This is our amazing custom theme.
+<?php
+// function myFirstFunction()
+// {
+//   echo "<p>Hello, this is my firts function.</p>";
+// }
+// myFirstFunction();
+// myFirstFunction();
+
+#########################
+function greet($name, $color)
+{
+  echo "<p>Hello, my is $name and my favorite color is $color.</p>";
+}
+
+greet("Milan", "green");
+greet("Darko", "red");
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
