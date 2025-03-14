78d57b07d357cec5227559252e4a45ece83b3490
Aleksandar Ristic <storiaca@gmail.com>
2025-03-14 16:14:43 +0100

    Header and footer start

--- fictional-university-theme/site/page.php
+++ fictional-university-theme/site/page.php
@@ -1,8 +1,10 @@
 <?php
+get_header();
 while (have_posts()) {
   the_post(); ?>
   <h1>This is a Page, not a Post</h1>
   <h2><?php the_title(); ?></h2>
   <?php the_content() ?>
 <?php }
+get_footer();
 ?>
