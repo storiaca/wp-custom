6a7523806474ff558c09b3eafe4ad2f792827238
Aleksandar Ristic <storiaca@gmail.com>
2025-03-31 19:33:56 +0200

    New files for site

--- fictional-university-theme/site/functions.php
+++ fictional-university-theme/site/functions.php
@@ -1,7 +1,12 @@
 <?php
 function university_files()
 {
-  wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+  wp_enqueue_style('font-roboto-condensed', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+  wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+
+
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
